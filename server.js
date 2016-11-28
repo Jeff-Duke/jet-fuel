e9d22d0608c5a00264cba056faf6291ba45e962c
Jeff Duke <jeff.duke@gmail.com>
2016-11-28 14:00:09 -0700

    works with fake URL

--- server.js
+++ server.js
@@ -4,6 +4,12 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 
+const urlChars = '123456789bcdfghjkmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ';
+
+app.locals.URLs = {
+  xZB32: 'http://www.turing.io',
+};
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -12,6 +18,13 @@ app.locals.title = 'Jet Fuel';
 
 app.get('/', (request, response) => {
   response.send('Let\'s shorten that URL');
+});
+
+app.get('/api/:shortURL', (request, response) => {
+  const { shortURL } = request.params;
+  let longURL = app.locals.URLs[shortURL];
+
+  response.json({ shortURL, longURL });
 });
 
 app.listen(app.get('port'), () => {
