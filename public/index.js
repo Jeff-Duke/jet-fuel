dcc66c21700eaede12e593fb561494aae6b2eaf0
Casey Cross <hellocaseycross@gmail.com>
2016-11-29 15:14:07 -0700

    getAllUrls function working in console.log

--- public/index.js
+++ public/index.js
@@ -1,6 +1,25 @@
-console.log('something');
-// const $ = require('jquery');
+
+const $urlInput = $('.url-input');
+const $displayUrls = $('.display-urls');
+
 
 $(document).ready(() => {
-  console.log('more things');
+  getAllUrls();
 });
+
+let getAllUrls = () => {
+  $.getJSON('api/URLs', (data) => {
+    putUrlsOnPage(data.URLs);
+  });
+
+};
+
+let putUrlsOnPage = (urls) => {
+  // let arr = Object.entries(urls);
+  //
+  // console.log(pizzas);
+
+  // Object.keys(urls).map((key, value) => {
+  //   console.log(key, value);
+  // });
+};
