186cffcd7809613ff5ccda19f5d4d21aaa2ac650
Casey Cross <hellocaseycross@gmail.com>
2016-11-29 16:29:22 -0700

    Urls now correctly rendering on page.

--- public/index.js
+++ public/index.js
@@ -1,4 +1,3 @@
-
 const $urlInput = $('.url-input');
 const $displayUrls = $('.display-urls');
 
@@ -11,15 +10,17 @@ let getAllUrls = () => {
   $.getJSON('api/URLs', (data) => {
     putUrlsOnPage(data.URLs);
   });
-
 };
 
-let putUrlsOnPage = (urls) => {
-  // let arr = Object.entries(urls);
-  //
-  // console.log(pizzas);
 
-  // Object.keys(urls).map((key, value) => {
-  //   console.log(key, value);
-  // });
+
+let putUrlsOnPage = (urls) => {
+  $.each(urls, (key,value) => {
+     $displayUrls.append(
+      `<div>
+        ${key} is short for ${value}
+      </div>`);
+  });
+
+
 };
